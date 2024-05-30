2de4ffe28d97b0aa680465cc46b3bec5cddf04b1
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2024-05-30 16:33:53 +0200

    chore: add consts file feat: display mock data feat: add opening of popover fix: rename admin to editor

--- pages/index.vue
+++ pages/index.vue
@@ -21,26 +21,126 @@
         gridTemplateRows: `repeat(${NUM_OF_ROWS}, ${SQUARE_DIMENSION})`,
       }"
     >
-      <div
-        v-for="cell in mockData"
-        :key="cell.id"
-        class="bg-blue-500"
+      <template
+        v-for="(_, y) in Array.from({ length: NUM_OF_ROWS })"
+        :key="y"
+        class="border border-black"
+      >
+        <div
+          v-for="(_, x) in Array.from({ length: NUM_OF_COLUMNS })"
+          :key="`${y} ${x}`"
+          class="relative"
+          :style="{
+            gridColumnStart: x + 1,
+            gridRowStart: y + 1,
+            gridColumnEnd: x + 2,
+            gridRowEnd: y + 2,
+          }"
+        >
+          <div
+            class="absolute left-1/4 top-1/4 h-1 w-1 rounded-full bg-black opacity-50"
+          ></div>
+          <div
+            class="absolute left-3/4 top-3/4 h-1 w-1 rounded-full bg-black opacity-50"
+          ></div>
+        </div>
+      </template>
+
+      <button
+        v-for="artwork in mockArtworks"
+        class="z-10 outline outline-2 outline-black"
+        :key="artwork.id"
         :style="{
-          background: 'blue',
-          gridColumnStart: cell.x,
-          gridRowStart: cell.y,
-          gridColumnEnd: cell.x + cell.spanX,
-          gridRowEnd: cell.y + cell.spanY,
+          gridColumnStart: artwork.x,
+          gridRowStart: artwork.y,
+          gridColumnEnd: artwork.x + artwork.spanX,
+          gridRowEnd: artwork.y + artwork.spanY,
         }"
-      ></div>
+        @click="openGroupPopover(artwork)"
+      >
+        <img
+          class="z-20 h-full w-full object-cover"
+          :src="`https://www.webumenia.sk/dielo/nahlad/${artwork.id}/600`"
+        />
+      </button>
     </div>
+  </div>
+  <div class="fixed inset-x-0 inset-y-0 z-10 bg-black/50" v-if="openedPopover">
+    <Popover
+      class="absolute bottom-0 right-0 h-[95%] w-1/3"
+      @close="closePopover"
+    >
+      <template v-slot:header>
+        <div class="flex flex-col">
+          <span>{{ openedPopover.artworks.length }} </span>
+          <span>{{ openedPopover.title }}</span>
+        </div>
+      </template>
+      <template v-slot:body> HERE GOES BODY </template>
+    </Popover>
   </div>
 </template>
 <script setup>
 import Logo from "~/assets/img/logo.svg?component";
-import { mockData } from "../data";
+import { NUM_OF_COLUMNS, NUM_OF_ROWS, SQUARE_DIMENSION } from "../consts";
 
-const SQUARE_DIMENSION = "104px";
-const NUM_OF_COLUMNS = 30;
-const NUM_OF_ROWS = 7;
+const openedPopover = ref(null);
+
+const openGroupPopover = (artwork) => {
+  const { group: groupId } = artwork;
+  const group = mockGroups.filter((group) => group.id === groupId)[0];
+
+  const artworks = mockArtworks.filter((artwork) =>
+    group.artworksIds.includes(artwork.id),
+  );
+
+  openedPopover.value = {
+    title: group.title,
+    intro: group.intro,
+    body: group.body,
+    artworks: artworks,
+  };
+};
+
+const closePopover = () => {
+  openedPopover.value = null;
+};
+
+const mockArtworks = [
+  { id: "SVK:SNG.UP-T_438", spanX: 1, spanY: 1, x: 2, y: 3, group: 1 },
+  { id: "SVK:SNG.UP-DK_75", spanX: 1, spanY: 1, x: 3, y: 1, group: 1 },
+  { id: "SVK:SNG.UP-F_1246", spanX: 1, spanY: 1, x: 1, y: 3, group: 1 },
+  { id: "SVK:SNG.UP-T_153", spanX: 1, spanY: 1, x: 5, y: 4, group: 1 },
+  { id: "SVK:SNG.UP-T_99", spanX: 1, spanY: 1, x: 8, y: 3, group: 2 },
+  { id: "SVK:SNG.UPS-K_15-M-2-3", spanX: 1, spanY: 1, x: 9, y: 1, group: 2 },
+  { id: "SVK:SNG.UP-F_1112", spanX: 1, spanY: 1, x: 10, y: 3, group: 2 },
+  { id: "SVK:SNG.UP-F_1258", spanX: 1, spanY: 1, x: 8, y: 4, group: 2 },
+];
+
+const mockGroups = [
+  {
+    id: 1,
+    artworksIds: [
+      "SVK:SNG.UP-T_438",
+      "SVK:SNG.UP-DK_75",
+      "SVK:SNG.UP-F_1246",
+      "SVK:SNG.UP-T_153",
+    ],
+    title: "The title of first group",
+    intro: "Random intro Rich text",
+    body: "Random Body Rich text",
+  },
+  {
+    id: 2,
+    artworksIds: [
+      "SVK:SNG.UP-T_99",
+      "SVK:SNG.UPS-K_15-M-2-3",
+      "SVK:SNG.UP-F_1112",
+      "SVK:SNG.UP-F_1258",
+    ],
+    title: "The title of second group",
+    intro: "Random intro Rich text II",
+    body: "Random Body Rich text",
+  },
+];
 </script>
