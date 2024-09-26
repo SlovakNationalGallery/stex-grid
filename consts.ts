9f29aa0b5cec19005fc334affc13caff91f0dd25
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2024-09-26 10:58:21 +0200

    fix: edge case slider

--- consts.ts
+++ consts.ts
@@ -1,6 +1,6 @@
 export const NUM_OF_ROWS = 7; // @TODO: Change this to 7 for the final version
 export const SQUARE_DIMENSION = `calc((100vh - 110px - 38px) / ${NUM_OF_ROWS})`;
-export const NUM_OF_COLUMNS = 158;
+export const NUM_OF_COLUMNS = 160;
 export const MAX_GRID_SCROLL = 1000;
 
   
